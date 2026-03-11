2194267f12f9f58f2ae8647c734598e0c47dc065
Developer <developer@chinamediguide.com>
2026-03-11 08:13:57 +0800

    fix: Remove loading import from locale layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,3 @@
-import { Suspense } from 'react'
-import Loading from './loading'
 import { LocaleLayout } from '@/components/layout/locale-layout'
 
 export default function LocaleLayoutWrapper({
@@ -9,9 +7,5 @@ export default function LocaleLayoutWrapper({
   children: React.ReactNode
   params: Promise<{ locale: string }>
 }) {
-  return (
-    <Suspense fallback={<Loading />}>
-      <LocaleLayout params={params}>{children}</LocaleLayout>
-    </Suspense>
-  )
+  return <LocaleLayout params={params}>{children}</LocaleLayout>
 }
